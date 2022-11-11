e8787521aa1c7211521c7a7b50e6998b56b54829
A01746459 <A01746459@itesm.mx>
2022-11-11 09:17:26 -0600

    Comentarios
    Se agregaron comentarios.

--- t/server.py
+++ t/server.py
@@ -1,20 +1,36 @@
+# -----
+# Descripción Breve:
+#   Esta es una simulación de multiagentes con el fin de estudiar las estadísticas 
+#   de un robot de limpieza reactivo.
+# -----
+# Autores:
+#   Adrián Bravo López A01752067
+#   Sebastián Burgos Alanís A01746459 
+# -----
+# Fecha de creación:
+#   10/11/22
+# Fecha de modificación:
+#   11/10/22
+# -----
+
 import mesa
 from mesa import Model
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid
 
+#-------------#
 #--- AGENT ---#
-class Cleaner(mesa.Agent):
-    """An agent that cleans dirty cells"""
+#-------------#
 
+class Cleaner(mesa.Agent):
+    # Clase para crear el Agente, definir su acción y su movimiento. 
+    # Constructor del agente de limpieza y sus atributos.
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
+    # Si la celda tiene Dirt, la limpia. 
     def step(self):
-        "Moves the cleaner to a random valid space"
         self.move()
-
-        "Checks for dirt in current cell, if so takes action"
         position = self.model.grid.get_cell_list_contents([self.pos])
         dirt = [obj for obj in position if isinstance(obj, Dirt)]
         if len(dirt) > 0:
@@ -22,8 +38,7 @@ class Cleaner(mesa.Agent):
             self.model.grid.remove_agent(dirt_remove)
             self.model.schedule.remove(dirt_remove)
 
-        
-    
+    # Función para mover al agente a una posición aleatoria disponible.
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(self.pos, True, True)
         chosen_step = self.random.choice(possible_steps)
@@ -32,13 +47,18 @@ class Cleaner(mesa.Agent):
         if len(cleaner) < 1:
             self.model.grid.move_agent(self, chosen_step)
 
-
 class Dirt(mesa.Agent):
+    # Constructor para la creación de la Dirt. 
     def __init__(self,unique_id,model):
         super().__init__(unique_id, model)
 
+#-------------#
 #--- MODEL ---#
+#-------------#
+
 class cleaningModel(Model):
+    # Clase para definir el Modelo, asiganción de variables, creación de 1 o más Agentes. 
+    # Constructor del Modelo con las variables requeridas
     def __init__(self,width,height,x,p,e):
         super().__init__()
         self.width = width
@@ -47,34 +67,39 @@ class cleaningModel(Model):
         self.schedule = mesa.time.RandomActivation(self)
         d = (p*(width*height))/100
 
-        #Create Cleaners
+        # Creación de los Agentes
         for i in range(x):
             clean = Cleaner(self.next_id(),self)
             self.schedule.add(clean)
             self.grid.place_agent(clean,(1,1))
             
-        
+        # ¿?
         for i in range(5):
             dirty = Dirt(self.next_id(),self)
             self.schedule.add(dirty)
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(dirty,(x,y))
-            
-
+        
     def step(self):
         self.schedule.step()
-#--- SERVEVR ---#
+
+#--------------#
+#--- SERVER ---#
+#--------------#
+
 def cleaning_port(agent):
+    # Función para crear el servidor, el Canvas, asignar el puerto de servidor,
+    #   definir los colores y figuras de los agentes así como la asiganción de los valores para 
+    #   crear Numero de Agentes, Espacio de habitación, Porcentaje de celdas sucias y tiempo de ejecución. 
     portrayal = {"Shape":"circle","Filled":"true", "r":0.5}
 
+    # Diseño de los Agentes
     if type(agent) is Cleaner:
-        
         portrayal["Color"] = "green"
         portrayal["Layer"] = 1
-        
     
-        print(agent.unique_id)
+    # Diseño de la Dirt. 
     else:
         portrayal["Color"] = "grey"
         portrayal["Layer"] = 1
@@ -82,8 +107,13 @@ def cleaning_port(agent):
 
     return portrayal
 
+# Diseño del Canvas y definición de valores. 
 grid = CanvasGrid(cleaning_port, 10, 10, 500, 500)
 server = ModularServer(cleaningModel,[grid],"cleaningModel",{"width":10,"height":10,"x":10,"p":25,"e":100})
+
+# Puerto de salida de transmisión. 
 server.port = 8521 
+
+# Lanzamiento del servidor. 
 server.launch()
 
